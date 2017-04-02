c004845c7e7710b09aecba34903db2e778563f2f
ZHENYU WANG <zhenyu2004@hotmail.com>
2017-04-02 15:21:21 +0800

    AVL tree

---
+++ AVLRoot/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct Node{
+  int Data;
+  int Height;
+  struct Node *Left,Right;
+
+
+};
+int main()
+{
+    printf("Hello world!\n");
+    int N;
+    int i;
+    scanf("%d",&N);
+    for(i=0;i<N;i++){
+        scanf("%d",&x);
+        Insert(T,x);
+    }
+    return 0;
+}
